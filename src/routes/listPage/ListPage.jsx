3be0f2b0aa4d080aa14f47d151696df1d0334a83
Junior-Osborn <juniorkhatib7@gmail.com>
2025-04-03 21:32:53 +0300

    Real Estate List

---
+++ src/routes/listPage/ListPage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./listPage.scss";
+import { listData } from "../../lib/dummyData";
+import Filter from "../../components/filter/Filter.jsx";
+import Card from "../../components/card/Card.jsx";
+const ListPage = () => {
+  const data = listData;
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer"></div>
+    </div>
+  );
+};
+
+export default ListPage;
